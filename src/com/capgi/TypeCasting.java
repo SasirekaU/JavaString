6f231a75b05443ace9a0e437891cb4ab10ed6a9a
SasirekaU <sasireka.umesh@gmail.com>
2022-02-03 09:53:36 +0530

    first commit

---
+++ src/com/capgi/TypeCasting.java
@@ -0,0 +1,36 @@
+package com.capgi;
+
+
+
+//Type Casting
+//convert from one data type to another data type
+//float -->int
+//double --float---int
+//String -->int 
+
+//
+
+public class TypeCasting {
+
+	public static void main(String arg[])
+	{
+  float f=1.0f;
+  //float--->int
+  int a=(int)f;
+  
+  //double -->int
+  double d=1.0;
+  //explicit type casting
+  int b=(int)d;
+  
+  //float -->f
+  //implicit type casting
+  
+   //small  -- big 
+  //Source --- >destination
+  double dd=(double)f;
+  
+  System.out.println(a);
+	}
+	
+}
